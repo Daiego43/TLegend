a9466c35501e3737a8f7ca7a0cc4d648a429f0ac
daiego <diedelcha@gmail.com>
2022-11-14 11:40:50 +0100

    Se crea una ventana y se destruye

--- model/workaround_confirm.py
+++ model/workaround_confirm.py
@@ -1,6 +1,9 @@
+import tkinter as tk
 from tkinter.messagebox import askyesno
 
 
 def confirm():
+    root = tk.Tk()
     answer = askyesno(title='Continue', message='press "Yes" to continue')
+    root.destroy()
     return answer
